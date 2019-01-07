eeb7e246e34d9eb24ed15fae0390f01d5836652d
hcghaha <huangchgu@163.com>
2019-01-07 21:26:33 +0800

    xxx

--- o2o-smart-study/src/test/java/com/huang/smart/study/test/concurrency/ThreadLocalVariableHolder.java
+++ o2o-smart-study/src/test/java/com/huang/smart/study/test/concurrency/ThreadLocalVariableHolder.java
@@ -44,7 +44,6 @@ class Accessor implements Runnable {
         id = idn;
     }
 
-    @Override
     public void run() {
         while(!Thread.currentThread().isInterrupted()){
             ThreadLocalVariableHolder.incement();
